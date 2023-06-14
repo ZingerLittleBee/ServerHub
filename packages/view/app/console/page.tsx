d8e0609f5ecd90f86e8d88cb3f714734d2db2e60
ZingerLittleBee <6970999@gmail.com>
2023-06-15 00:54:46 +0800

    feat: 🎸 dashboards lib

--- packages/view/app/console/page.tsx
+++ packages/view/app/console/page.tsx
@@ -1,6 +1,14 @@
 
-export default function IndexPage() {
-  return (
-<div>console</div>
-  )
-}
+import { Card, Text, Metric, Flex, ProgressBar } from "@tremor/react";
+
+export default () => (
+  <Card className="max-w-xs mx-auto">
+    <Text>Sales</Text>
+    <Metric>$ 71,465</Metric>
+    <Flex className="mt-4">
+      <Text>32% of annual target</Text>
+      <Text>$ 225,000</Text>
+    </Flex>
+    <ProgressBar value={32} className="mt-2" />
+  </Card>
+);
