a2435569fbda49d4bfcfc3faf5a12821795d82ab
ZingerLittleBee <6970999@gmail.com>
2023-06-15 14:32:23 +0800

    feat: 🎸 env

--- packages/app/src/auth/auth.module.ts
+++ packages/app/src/auth/auth.module.ts
@@ -1,20 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import {UserModule} from "@/user/user.module";
-import {JwtModule} from "@nestjs/jwt";
-import {ConfigModule, ConfigService} from "@nestjs/config";
+import { UserModule } from '@/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule,
+  imports: [
+    UserModule,
     JwtModule.registerAsync({
       global: true,
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: { expiresIn: '60s' },
-      }),
+      useFactory: async (configService: ConfigService) => {
+        let expiresIn = '86400s';
+        if (configService.get('JWT_EXPIRATION_TIME')) {
+          expiresIn = `${configService.get('JWT_EXPIRATION_TIME')}s`;
+        }
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: expiresIn },
+        };
+      },
     }),
   ],
   controllers: [AuthController],
